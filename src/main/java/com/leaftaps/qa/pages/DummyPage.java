998b0cd34b41a44f78dc8a97becdc9f2c0137e13
HemantDhavle <96099050+HemantDhavle@users.noreply.github.com>
2021-12-25 15:35:34 +0530

    Create DummyPage.java

---
+++ src/main/java/com/leaftaps/qa/pages/DummyPage.java
@@ -0,0 +1,13 @@
+package com.leaftaps.qa.pages;
+
+public class DummyPage 
+{
+	public void dummymethod()
+	{
+    int a = 10;
+    int b = 20;
+    System.out.println(a+b);
+		System.out.println("this is my dummy method");
+	}
+
+}
